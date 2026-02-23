f024639022a9c21a3018bacb1950f663176075c1
amaiavaz <amaiavzqz@gmail.com>
2026-02-23 10:22:34 +0100

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,7 +7,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|login|unauthorized).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth|unauthorized).*)'],
 };
